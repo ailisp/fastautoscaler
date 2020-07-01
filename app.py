be6ca24d4b397db5444fd9af7d54a31093c5c9dc
Bo Yao <bo@nearprotocol.com>
2020-07-01 16:35:05 -0700

    fix typo

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def create_machine(item):
         init_script = item.get('init_script')
         if init_script:
             machines[name] = {
-                'status': 'initializing', **item, ip:''
+                'status': 'initializing', **item, 'ip':''
             }
             p = getattr(rc, mg['provider']).get(name).bash(f'set -euo pipefail\n{init_script}')
             if p.returncode != 0:
